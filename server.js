6463d791d590a4ee6dbc583ab655449cd1ce83d0
flamelmamel <211101@astanait.edu.kz>
2022-05-18 21:19:48 +0600

    gg

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist')));
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist', 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
 app.listen(port);
